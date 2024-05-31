1f66e3ca5266fc499287eaa17e3c775ef9fbf215
Muhammad Ahmed Villa Khan <m.ahmedk287@gmail.com>
2024-05-31 18:46:15 +0500

    feat: adding new models and dto

---
+++ Models/UserRole.cs
@@ -0,0 +1,11 @@
+﻿namespace identity_jwt.Models
+{
+    /// <summary>
+    /// Static class defining available user roles in the application
+    /// </summary>
+    public static class UserRoles
+    {
+        public const string Admin = "Admin";
+        public const string User = "User";
+    }
+}
