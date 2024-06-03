a4fc83bed3d34819e1a2ef6c3e8876a23bc633ee
Muhammad Ahmed Villa Khan <m.ahmedk287@gmail.com>
2024-06-03 18:27:53 +0500

    chore: added a dummy controller and auth controller

---
+++ Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace identity_jwt.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class EmployeeController : ControllerBase
+    {
+        [HttpGet]
+        public async Task<IActionResult> Get()
+        {
+            var employees = await Task.FromResult(new string[] { "ahmed", "adam", "bob" });
+            return Ok(employees);
+        }
+    }
+}
